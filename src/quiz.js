125e3df6509219cb3f7b1d03852a088d542c89d6
WildFont <jordifont4@gmail.com>
2026-04-07 16:32:13 +0200

    Next steps

--- src/quiz.js
+++ src/quiz.js
@@ -3,11 +3,16 @@ class Quiz {
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
-    constructor(questions, timeLimit, timeRemaining) {
-    this.questions = questions;
-    this.timeLimit = timeLimit;
-    this.timeRemaining = timeRemaining;
-  }
+   constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
+   getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+    }
 }
 
 
