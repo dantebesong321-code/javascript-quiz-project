c92831b92eafaa6159961bfd4b353207d8b7ee2c
WildFont <jordifont4@gmail.com>
2026-04-07 17:28:35 +0200

    day one and two

--- src/quiz.js
+++ src/quiz.js
@@ -1,31 +1,57 @@
 class Quiz {
-    
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
-   constructor(questions, timeLimit, timeRemaining) {
-        this.questions = questions;
-        this.timeLimit = timeLimit;
-        this.timeRemaining = timeRemaining;
-        this.correctAnswers = 0;
-        this.currentQuestionIndex = 0;
-    }
-   getQuestion() {
-        return this.questions[this.currentQuestionIndex];
-    }
-}
-
-
-   
   // 1. constructor (text, choices, answer, difficulty)
-  
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
+  // 2. getQuestion()
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+  // 3. moveToNextQuestion()
 
-    // 4. shuffleQuestions()
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
+  // 4. shuffleQuestions()
 
-    // 5. checkAnswer(answer)
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.questions[i], this.questions[j]] = [
+        this.questions[j],
+        this.questions[i],
+      ];
+    }
+  }
+  // 5. checkAnswer(answer)
 
-    // 6. hasEnded()
+  checkAnswer(answer) {
+    if (answer === this.getQuestion().answer) {
+      this.correctAnswers++;
+    }
+  }
+  // 6. hasEnded()
+  hasEnded() {
+    return this.currentQuestionIndex === this.questions.length;
+  }
+
+  filterQuestionsByDifficulty(difficulty) {
+    if (difficulty >= 1 && difficulty <= 3) {
+      this.questions = this.questions.filter(
+        (q) => q.difficulty === difficulty,
+      );
+    }
+  }
+
+  averageDifficulty() {
+    return (
+      this.questions.reduce((sum, q) => sum + q.difficulty, 0) /
+      this.questions.length
+    );
+  }
+}
